dcfefb500fc4315215be06b833bfd124a190e05a
Daniel Bolude <boludedaniel@gmail.com>
2024-03-27 08:41:20 +0100

    added course reg table and dummy payment

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -6,16 +6,33 @@ import {
   CardHeader,
   CardTitle,
 } from "../../components/ui/card";
-import { Button } from "../../components/ui/button";
 import { Course, Level, levelCourseData } from "./data";
 import useUserStore from "../store/useUserStore";
 import { toast } from "sonner";
 import { CheckCircledIcon, CrossCircledIcon } from "@radix-ui/react-icons";
-
+import { Label } from "../../components/ui/label";
+import { Input } from "../../components/ui/input";
+import { Button } from "../../components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../../components/ui/table";
 const Register = () => {
   const { username, matricNo, registeredCourses, setRegisteredCourses } =
     useUserStore();
   const [currentCourses, setCurrentCourses] = React.useState<Course[]>();
+  const [firstSemesterCourses, setFirstSemesterCourses] =
+    React.useState<Course[]>();
+  const [secondSemesterCourses, setSecondSemesterCourses] =
+    React.useState<Course[]>();
+  const [currentScene, setCurrentScene] = React.useState<
+    "Paid" | "Unpaid" | "Preview"
+  >("Unpaid");
 
   const handleSelectLevel = (
     course: Level[]
@@ -45,6 +62,9 @@ const Register = () => {
     const secondSemesterCourses = registeredCourses.filter(
       (course: Course) => course.semester === "2nd"
     );
+
+    setFirstSemesterCourses(firstSemesterCourses);
+    setSecondSemesterCourses(secondSemesterCourses);
 
     const firstSemesterCreditLoad = firstSemesterCourses.reduce(
       (acc: number, course: Course) => acc + course.creditLoad,
@@ -119,52 +139,234 @@ const Register = () => {
         <h3 className="text-xl">Welcome, {username} </h3>
         <p className="text-xl">Matric No - {matricNo}</p>
       </div>
-      <div className="h-[80%] flex items-center justify-center">
-        <form>
-          <Card className="max-w-[1000px]  ">
-            <div className="flex w-full items-center justify-between font-mono py-2"></div>
 
-            <CardHeader className="py-8">
-              <CardTitle className="font-spaceGrotesk text-lg ">
-                <h1 className="text-2xl"> Register Your Courses</h1>
-              </CardTitle>
-            </CardHeader>
+      {currentScene === "Unpaid" && (
+        <div className="h-[80%] flex items-center justify-center">
+          <form>
+            <Card className="w-[600px] h-[50vh]">
+              <div className="flex w-full items-center justify-between font-mono py-2"></div>
 
-            <div className="flex  flex-col w-full justify-center items-center  gap-4">
-              <CardContent className="space-y-2 w-full font-light">
-                <div className="space-y-1 w-full font-jakarta">
-                  <select
-                    className="w-[95%] bg-gray-100 border border-black/20 py-2 rounded-sm"
-                    onChange={selectHandler}
-                  >
-                    <option value="" disabled selected hidden>
-                      Please select your current level
-                    </option>
+              <CardHeader className="py-8">
+                <CardTitle className=" text-lg ">
+                  <h1 className="text-2xl"> Make Payment</h1>
+                </CardTitle>
+              </CardHeader>
 
-                    <option value="100 level">100 level</option>
-                    <option value="200 level"> 200 level</option>
-                    <option value="300 level"> 300 level</option>
-                    <option value="400 level"> 400 level</option>
-                    <option value="500 level">500 level</option>
-                  </select>
-                </div>
+              <div className="flex w-full justify-center  gap-2">
+                <CardContent className="space-y-2 w-full font-light">
+                  <div className="space-y-1 font-jakarta">
+                    <Label
+                      className="flex flex-col gap-3 text-md"
+                      htmlFor="product-code "
+                    >
+                      Name
+                      <Input
+                        className="font-normal text-md"
+                        placeholder="Pay ₦39950"
+                      />
+                    </Label>
+                  </div>
+                </CardContent>
+              </div>
+
+              <CardFooter>
+                <Button
+                  onClick={(e) => {
+                    {
+                      e.preventDefault();
+
+                      setCurrentScene("Paid");
+                      toast(`Payment Successful`, {
+                        className:
+                          "font-mono text-sm text-start   py-4 flex items-center justify-center border border-black h-[6rem]",
+                        duration: 4000,
+                        icon: <CheckCircledIcon />,
+                      });
+                    }
+                  }}
+                  type="submit"
+                  className=" text-md font-jakarta h-9 px-10"
+                >
+                  Make Payment
+                </Button>
+              </CardFooter>
+            </Card>
+          </form>
+        </div>
+      )}
+      {currentScene === "Paid" && (
+        <div className="h-[80%] flex items-center justify-center">
+          <form>
+            <Card className=" max-w-[400px]  md:max-w-[1000px]  ">
+              <div className="flex w-full items-center justify-between font-mono py-2"></div>
+
+              <CardHeader className="py-8">
+                <CardTitle className="font-spaceGrotesk text-lg ">
+                  <h1 className="text-2xl"> Register Your Courses</h1>
+                </CardTitle>
+              </CardHeader>
+
+              <div className="flex  flex-col w-full justify-center items-center  gap-4">
+                <CardContent className="space-y-2 w-full font-light">
+                  <div className="space-y-1 w-full font-jakarta">
+                    <select
+                      className="w-[95%] bg-gray-100 border border-black/20 py-2 rounded-sm"
+                      onChange={selectHandler}
+                    >
+                      <option value="" disabled selected hidden>
+                        Please select your current level
+                      </option>
+
+                      <option value="100 level">100 level</option>
+                      <option value="200 level"> 200 level</option>
+                      <option value="300 level"> 300 level</option>
+                      <option value="400 level"> 400 level</option>
+                      <option value="500 level">500 level</option>
+                    </select>
+                  </div>
+                </CardContent>
+
+                <CardContent className=" rounded-sm items-center justify-start   overflow-hidden flex flex-col gap-4 bg-gray-100   w-[90%]">
+                  <div className=" h-[15rem] flex flex-col gap-5 overflow-y-scroll">
+                    {levelOptions}
+                  </div>
+                </CardContent>
+              </div>
+
+              <CardFooter className="py-4">
+                <Button
+                  onClick={() => {
+                    setCurrentScene("Preview");
+                  }}
+                  type="submit"
+                  className=" text-md bg-black text-white font-jakarta h-9 px-10"
+                >
+                  Register
+                </Button>
+              </CardFooter>
+            </Card>
+          </form>
+        </div>
+      )}
+
+      {currentScene === "Preview" && (
+        <div className="h-[80%] flex items-center justify-center">
+          <form>
+            <Card className=" max-w-[400px]  md:max-w-[1000px] xl:w-[800px]  ">
+              <div className="flex w-full items-center justify-between font-mono py-2"></div>
+
+              <CardHeader className="py-8">
+                <CardTitle className="font-spaceGrotesk text-lg ">
+                  <h1 className="text-2xl"> Preview Your Registered Courses</h1>
+                </CardTitle>
+              </CardHeader>
+
+              <CardContent className="overflow-y-scroll">
+                <Table className="">
+                  <TableCaption>
+                    First Semester Courses
+                    <h3 className="mr-4 text-black font-semibold">
+                      Credit Units:{" "}
+                      {currentCourses
+                        ?.filter((course: Course) => course.semester === "1st")
+                        .reduce(
+                          (acc: number, course: Course) =>
+                            acc + course.creditLoad,
+                          0
+                        )}
+                    </h3>
+                  </TableCaption>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead className="w-[100px]">Course Code</TableHead>
+                      <TableHead>Course Title</TableHead>
+                      <TableHead>Credit Load</TableHead>
+                      <TableHead className="text-right">Semester</TableHead>
+                      <TableHead className="text-right">Nature</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody className="overflow-y-scroll">
+                    {currentCourses
+                      ?.filter((course: Course) => course.semester === "1st")
+                      .map((course, i) => {
+                        return (
+                          <TableRow key={i}>
+                            <TableCell className="font-medium">
+                              {course?.courseCode}
+                            </TableCell>
+                            <TableCell>{course?.courseTitle} </TableCell>
+                            <TableCell>{course.creditLoad}</TableCell>
+                            <TableCell className="text-right">
+                              {course.semester}
+                            </TableCell>
+                            <TableCell className="text-right">
+                              {course.nature}
+                            </TableCell>
+                          </TableRow>
+                        );
+                      })}
+                  </TableBody>
+                </Table>
+
+                <Table>
+                  <TableCaption className="  w-full">
+                    2nd Semester Courses
+                    <h3 className="mr-4 text-black font-semibold">
+                      Credit Units:
+                      {currentCourses
+                        ?.filter((course: Course) => course.semester === "2nd")
+                        .reduce(
+                          (acc: number, course: Course) =>
+                            acc + course.creditLoad,
+                          0
+                        )}
+                    </h3>
+                  </TableCaption>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead className="w-[100px]">Course Code</TableHead>
+                      <TableHead>Course Title</TableHead>
+                      <TableHead>Credit Load</TableHead>
+                      <TableHead className="text-right">Semester</TableHead>
+                      <TableHead className="text-right">Nature</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody className="overflow-y-scroll">
+                    {currentCourses
+                      ?.filter((course: Course) => course.semester === "2nd")
+                      .map((course, i) => {
+                        return (
+                          <TableRow key={i}>
+                            <TableCell className="font-medium">
+                              {course?.courseCode}
+                            </TableCell>
+                            <TableCell>{course?.courseTitle} </TableCell>
+                            <TableCell>{course.creditLoad}</TableCell>
+                            <TableCell className="text-right">
+                              {course.semester}
+                            </TableCell>
+                            <TableCell className="text-right">
+                              {course.nature}
+                            </TableCell>
+                          </TableRow>
+                        );
+                      })}
+                  </TableBody>
+                </Table>
               </CardContent>
 
-              <CardContent className=" rounded-sm items-center justify-start   overflow-hidden flex flex-col gap-4 bg-gray-100   w-[90%]">
-                <div className=" h-[15rem] flex flex-col gap-5 overflow-y-scroll">
-                  {levelOptions}
-                </div>
-              </CardContent>
-            </div>
-
-            <CardFooter className="py-4">
-              <Button type="submit" className=" text-md font-jakarta h-9 px-10">
-                Register
-              </Button>
-            </CardFooter>
-          </Card>
-        </form>
-      </div>
+              <CardFooter className="py-4">
+                <Button
+                  onClick={() => {}}
+                  className=" text-md bg-black text-white font-jakarta h-9 px-10"
+                >
+                  Print
+                </Button>
+              </CardFooter>
+            </Card>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
